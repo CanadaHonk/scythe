349aceeb828cda00b255c8b44bc0718bd075b642
Oj <oj@oojmed.com>
2021-01-19 14:24:33 +0000

    [Logging] Better final output logging

--- src/index.js
+++ src/index.js
@@ -6,4 +6,8 @@ const mainJSFile = process.argv[2];
 const code = readFileSync(mainJSFile, 'utf8');
 const out = await replaceES6Imports(code, mainJSFile);
 
-console.log(`\n\n${code}`, `\n\n${out}`);
+console.log(`\n${'-'.repeat(20)} original ${'-'.repeat(20)}
+${code}
+
+${'-'.repeat(20)}  output  ${'-'.repeat(20)}
+${out}`);
